de4777694f79e3b4522ac35e493da1b9070292f4
Amir <amir.mohtasebi@gmail.com>
2015-08-22 21:09:38 +1200

    added grunt watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,38 +1,50 @@
 module.exports = function (grunt) {
-  'use strict';
+    'use strict';
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-jslint');
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    jslint: {
-        client: {
-            src: ['File/*.js'],
-            exclude: [],
-            directives: {
-                browser: true,
-                white: true,
-                vars: true,
-                predef: [
-                    "angular",
-                    "$",
-                    "document",
-                    "FileReader",
-                    "console",
-                    "window",
-                    "module"
-                ]
-            }
-        }
-    },
-    jshint: {
-        files: ['File/*.js'],
-        options: {
-            jshintrc: true
-        }
-    }
-  });
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-jslint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
   
-  grunt.registerTask('default', ['jslint', 'jshint']);
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jslint: {
+            client: {
+                src: ['File/*.js'],
+                exclude: [],
+                directives: {
+                    browser: true,
+                    white: true,
+                    vars: true,
+                    predef: [
+                        "angular",
+                        "$",
+                        "document",
+                        "FileReader",
+                        "console",
+                        "window",
+                        "module"
+                    ]
+                }
+            }
+        },
+        jshint: {
+            files: ['File/*.js'],
+            options: {
+                jshintrc: true
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['File/*.js'],
+                tasks: ['default']
+            }
+        }
+    });
+
+    grunt.registerTask('default', ['jslint', 'jshint']);
+    
+    grunt.event.on('watch', function(action, filepath) {
+        // grunt.log.write(action);
+    });
 };
