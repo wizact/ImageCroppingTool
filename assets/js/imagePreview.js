21d28135615dc3aefd3b24fc2a29fd0ae4cdb052
Amir <amir.mohtasebi@gmail.com>
2015-08-30 01:55:47 +1200

    functionality to set image preview original sizes and placement of preview image in center

--- assets/js/imagePreview.js
+++ assets/js/imagePreview.js
@@ -29,12 +29,22 @@ document.addEventListener('mouseup', handleMouseUp, false);
 
 function ImagePreview(previewElement, options) {
     'use strict';
+    // Drop zone size
     this.width = 300;
     this.height = 30;
+
+    // Preview image size
+    this.previewWidth = 300;
+    this.previewHeight = 30;
 
     if (options !== undefined) {
         this.width = options.width || this.width;
         this.height = options.height || this.height;
+    }
+
+    if (options !== undefined) {
+        this.previewWidth = options.previewWidth || this.previewWidth;
+        this.previewHeight = options.previewHeight || this.previewHeight;
     }
 
     this.previewElement = previewElement;
@@ -66,39 +76,36 @@ ImagePreview.prototype.load = function(imageData) {
     var img = new Image();
     img.setAttribute("id", "img1");
     img.setAttribute("class", "imgToResize");
-    
-    // default size of image after load
-    //img.setAttribute("width",  "100px");
-    //img.setAttribute("height", "100px");
+    img.setAttribute("draggable", "false");
 
     img.onload = function() {
-        
-        /*
         var imgWidth = img.width,
             imgHeight = img.height,
-            newWidth = this.width,
-            newHeight = this.height,
+            newWidth = this.previewWidth,
+            newHeight = this.previewHeight,
             ratio = 1;
 
         if (imgWidth > imgHeight) {
-            ratio = imgWidth / this.width;
+            ratio = imgWidth / this.previewWidth;
         }  else {
-            ratio = imgHeight / this.height;
+            ratio = imgHeight / this.previewHeight;
         }
 
         newHeight = imgHeight / ratio;
         newWidth = imgWidth / ratio;
 
-        console.log("%s => %s", imgWidth, newWidth);
-        console.log("%s => %s", imgHeight, newHeight);
-        console.log(ratio);
+        // default size of image after load
+        img.setAttribute("width",  newWidth + "px");
+        img.setAttribute("height", newHeight + "px");
 
+        // Move the preview to the center of the drop zone
+        img.style.left = Math.floor((this.width / 2) - (newWidth /2)) + "px";
+        img.style.top = Math.floor((this.height / 2) - (newHeight /2)) + "px";
+        
+        /*
         ctx.drawImage(img, 0 , 0, newWidth, newHeight);
         */
         document.getElementById(this.previewElement).insertBefore(img, null);
-
-
-        //var col = document.getElementById('#img1');
 
         img.addEventListener('mousedown', handleMouseDown, false);
     }.bind(this);
