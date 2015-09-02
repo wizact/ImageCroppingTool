578b755f99504a53013c8b4881c05d0431a2218c
Amir <amir.mohtasebi@gmail.com>
2015-09-02 22:59:50 +1200

    added unit tests for canvasEventModule

--- test/imagePreview_test.js
+++ test/imagePreview_test.js
@@ -5,8 +5,6 @@ var vm = require('vm');
 
 
 describe('imagePreview', function() {
-    describe('createHandleBars()', function() {
-
     before(function() {
         var doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
         var win = doc.defaultView;
@@ -27,13 +25,48 @@ describe('imagePreview', function() {
 
         var code = fs.readFileSync('./assets/js/imagePreview.js');
         vm.runInThisContext(code);
-        });
+    });
+
+    describe('createHandleBars()', function() {
 
     it('should return element with correct id and class list', function() {
         var ip = new ImagePreview('drop_file2');
         var hb = ip.createHandleBars('name', 'classA classB');
-        console.log(hb.id);
-        assert.equal('drop_file2_name', hb.id);
+        assert.equal(hb.id, 'drop_file2_name');
         });
     });
+
+    it('handle mouse up should reset the state', function() {
+        var module = canvasEventModule;
+        module.moveState.imageMoveState = true;
+        module.resizeState.imageResizeState = true;
+
+        module.handleMouseUp();
+
+        assert.equal(false, module.moveState.imageMoveState);
+        assert.equal(false, module.resizeState.imageResizeState);
+    });
+
+    function isHandleBarTest() {
+        var module = canvasEventModule;
+        return module.isHandleBar.call(arguments);
+    }
+
+    it('should identify handlebars by element type', function() {
+
+        var tests = [
+            { args: 'se', expected: true },
+            { args: 'sw', expected: true },
+            { args: 'ne', expected: true },
+            { args: 'nw', expected: true }
+        ];
+
+        tests.forEach(function(test) {
+            it('identity ' + test.args + ' as handlebar', function() {
+                var actual = isHandleBarTest.apply(null, test.args);
+                assert.equal(actual, tests.expected);
+            });
+        });
+    });
+
 });
