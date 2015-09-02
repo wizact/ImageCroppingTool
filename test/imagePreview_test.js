a4e86f6a2bec2980ddab85cfbc1e6759e9d70c2f
Amir <amir.mohtasebi@gmail.com>
2015-09-02 20:28:34 +1200

    added jsdom and test structure

--- test/imagePreview_test.js
+++ test/imagePreview_test.js
@@ -1,11 +1,39 @@
-var assert = require("assert");
-var objUnderTest = require("../assets/js/imagePreview.js");
+var assert = require('assert');
+var jsdom = require('jsdom');
+var fs = require('fs');
+var vm = require('vm');
+
+
 describe('imagePreview', function() {
     describe('createHandleBars()', function() {
-        it('should return element with correct id and class list', function() {
-            var ip = new ImagePreview('drop_file2');
-            var hb = ip.createHandleBars('name', 'classA classB');
-            assert.equal('drop_file2_name', hb.id);
+
+    before(function() {
+        var doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
+        var win = doc.defaultView;
+
+        global.document = doc;
+        global.window = win;
+
+        propagateToGlobal(win);
+
+        function propagateToGlobal (window) {
+            for (var key in window) {
+                if (!window.hasOwnProperty(key)) continue
+                if (key in global) continue
+
+                global[key] = window[key]
+            }
+        }
+
+        var code = fs.readFileSync('./assets/js/imagePreview.js');
+        vm.runInThisContext(code);
+        });
+
+    it('should return element with correct id and class list', function() {
+        var ip = new ImagePreview('drop_file2');
+        var hb = ip.createHandleBars('name', 'classA classB');
+        console.log(hb.id);
+        assert.equal('drop_file2_name', hb.id);
         });
     });
 });
